94a573ce5d3d40c834e107116d664bf35f4dfa5e
Ruslan <ruslan.b1976@gmail.com>
2019-08-29 05:52:53 -0400

    A working home page with few library stats

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from catalog.models import Author, Book, BookInstance, Genre, Language
 
-# Register your models here.
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
+from catalog.models import Author, Book, BookInstance, Genre
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of the main objects
+    num_books = Book.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status == 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+    }
+
+    return render(request, 'index.html', context=context)
